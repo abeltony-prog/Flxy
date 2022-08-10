9ca6abe427d59799c7951b44bbea41144e3cf592
AbelTony <abeltony03@gmail.com>
2022-08-10 13:43:13 +0200

    Engineer page edit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+
+export default NextAuth({
+
+  providers: [
+    Google({
+      secret:process.env.SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import { Grid } from "@mui/material";
+import { useSession } from "next-auth/react";
 import CaseStatus from "../src/components/dashboard/CaseStatus";
 import MonthlyOverview from "../src/components/dashboard/MonthlyOverview";
 import ElevationStatus from "../src/components/dashboard/ElevationStatus";
 
+
 export default function Index() {
+  const { data: session } = useSession();
+  // const { data: userInfo } = useGetUserInoQuery({
+  //   _eq: session?.user?.email,
+  // });
+
+  console.log(session)
+  if(session?.user = null){
+    return <p>hello</p>
+  }else{
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
         <MonthlyOverview />
       </Grid>
-      {/* ------------------------- row 1 ------------------------- */}
       <Grid item xs={12} lg={4}>
         <ElevationStatus />
       </Grid>
@@ -18,4 +28,5 @@ export default function Index() {
       </Grid>
     </Grid>
   );
+  }
 }
